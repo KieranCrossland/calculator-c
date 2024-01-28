91f0c5b67d5581c21028e64c5a3534efe369d660
KieranCrossland <85656267+KieranCrossland@users.noreply.github.com>
2024-01-28 00:55:52 +0000

    improved code + fixed crossplatform

--- src/calculator.c
+++ src/calculator.c
@@ -1,96 +1,90 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
+#include <stdlib.h>
 #include <signal.h>
 
-void catch_int(int sig_num) 
-{
+#define VERSION 0.4
+#define NAME "calculator-c"
+#define REPO_URL "https:/github.com/kierancrossland/calculator-c"
+
+void catch_int(int sig_num) {
     signal(SIGINT, catch_int);
     printf("\n\nCaught CTRL-C SIGINT\n");
     fflush(stdout);	
-    exit(1);
+	exit(0);
 
 }
 
-int main(int argc, char *argv[])
-{
-	const char NAME[] = "calculator";
-	const float VERSION = 0.3;
+int main(int argc, char *argv[]) {
 
-    if (argc > 1)
-    {
+    if (argc > 1) {
         if (strcmp(argv[1],"-v") == 0) {
-		printf("%s %.1f",NAME, VERSION);
-			exit(EXIT_SUCCESS);
+		printf("%s %.1f\n",NAME, VERSION);
+			return 0;
 		}
 		if (strcmp(argv[1],"-r") == 0) {
-			printf("Attempting to open default browser (xdg-open)\n");
-			system("xdg-open https:/github.com/kierancrossland/calculator-c");
-			exit(EXIT_SUCCESS);
+			#ifdef __APPLE__
+			system("open "REPO_URL);
+			#endif
+			#ifdef _WIN32
+			system("start "REPO_URL);
+			#else
+			system("xdg-open "REPO_URL);
+			#endif
+			return 0;
 		} 
-		if (strcmp(argv[1],"-w") == 0) {
-			printf("Attempting to open default browser (xdg-open)\n");
-			system("xdg-open https://www.kieranc.xyz");
-			exit(EXIT_SUCCESS);
-		}
-		if (strcmp(argv[1],"-m") == 0) {
-			printf("Attempting to open default Email client (xdg-email)\n");
-			system("xdg-email mailto:kierancrossland1st@gmail.com");
-			exit(EXIT_SUCCESS);
-		}
 		if (strcmp(argv[1],"-h") == 0) {
 			help_arg();
-			exit(EXIT_SUCCESS);
+			return 0;
 		}
 		else {
 			printf("Error! Invalid argument \"%s\" ",argv[1]);
 			help_arg();
-			exit(EXIT_FAILURE);
+			return 1;
 		}
 }
 	calculator();
 }
-int help_arg() 
-{
+int help_arg() {
 	printf("Avaliable arguments:\n");
 	printf("  -v\tPrints the version information\n");
 	printf("  -r\tOpens the program's Git repository\n");
-	printf("  -w\tOpens the programmer's website\n");
-	printf("  -m\tEmails the programmer (kierancrossland1st@gmail.com)\n");
+	printf("  -h\tShows arguments\n");
 }
 
-int calculator()  
-{
+void calculator() {
 	signal(SIGINT, catch_int); 
 
 	char operator[1];	
-	int number1; 		
-	int number2;	
-	
-	printf("Enter expression: ");
-	scanf("%d",&number1);
-	scanf("%s",operator);
-	scanf("%d",&number2);
-	printf("%d %s %d",number1,operator,number2);
-	
-	if (strcmp(operator, "+") == 0) { 		
-		int sum = number1 + number2;
-		printf(" = %d\n", sum);
-		calculator();
-	}
-	else if (strcmp(operator, "-") == 0) {	
-		int difference = number1 - number2;
-		printf(" = %d\n",difference);
-	 	calculator();
-	}
-	else if (strcmp(operator, "*") == 0) {	
-		int product = number1 * number2;
-		printf(" = %d\n",product);
-	 	calculator();
-	}
-	else if (strcmp(operator, "/") == 0) {	
-		int quotient = number1 / number2;
-		printf(" = %d\n",quotient);
-	 	calculator();
+	int num1; 		
+	int num2;
+
+	while (true) {
+		printf("Enter expression: ");
+		scanf("%d",&num1);
+		scanf("%s",operator);
+		scanf("%d",&num2);
+		
+		printf("%d %s %d ",num1,operator,num2);
+
+		if (num2 == 0 && operator[0] == '/') {
+			printf("cannot divide by 0.\n");
+			break;
+		}
+		switch (operator[0]) {
+			case '+':
+				printf("= %d\n", num1 + num2);
+				break;
+			case '-':
+				printf("= %d\n", num1 - num2);
+				break;
+			case '/':
+				printf("= %d\n", num1 / num2);
+				break;
+			case '*':
+				printf("= %d\n", num1 * num2);
+				break;
+		}
 	}
 }
